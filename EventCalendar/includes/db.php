c343b22a8c9b21bc0ccf66451458b199653acdd8
hopedevelopers254 <design.hopedevelopers@gmail.com>
2025-02-17 13:34:25 +0300

    Added EventCalendar as a normal folder

---
+++ EventCalendar/includes/db.php
@@ -0,0 +1,15 @@
+<?php
+// Database connection details
+$servername = "localhost";
+$username = "root";
+$password = "22092209";
+$dbname = "event_calendar";
+
+try {
+    // Create a new PDO instance and set the error mode to exception
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    // Handle connection failure
+    echo "Connection failed: " . $e->getMessage();
+}
